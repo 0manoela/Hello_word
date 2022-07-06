018becf987e8b81eef797fad6174c865b1b0c337
0manoela <106315497+0manoela@users.noreply.github.com>
2022-07-06 14:33:13 -0300

    ATIVIDADE – Spring

---
+++ helloword/src/main/java/com/helloword/helloword/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.helloword.helloword.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-word")
+public class HelloWorldController {
+	
+		@GetMapping
+		public String helloWorld() {
+			return "Hello World!!!";
+		}
+}
